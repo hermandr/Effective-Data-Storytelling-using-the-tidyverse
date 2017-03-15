103551a51200e0ab16af952e394796887baeaa95
Chester Ismay <chester.ismay@gmail.com>
2017-03-15 15:20:17 -0700

    Update 538 package to 0.2.0

--- requirements.r
+++ requirements.r
@@ -1,7 +1,7 @@
 devtools::install_version("tidyverse", "1.1.0", repos = "http://cran.us.r-project.org")
 devtools::install_version("nycflights13", "0.2.1")
 devtools::install_version("okcupiddata", "0.1.0")
-devtools::install_version("fivethirtyeight", "0.1.0")
+devtools::install_version("fivethirtyeight", "0.2.0")
 devtools::install_version("babynames", "0.2.1")
 devtools::install_version("mosaic", "0.14.4")
 devtools::install_version("ggplot2movies", "0.0.1")
